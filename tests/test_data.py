428419079aae8099e516de075967ce96b7e83298
karlfindhansen <karl.find.hansen@gmail.com>
2025-01-16 14:41:38 +0100

    commit some folders

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,10 @@
 import torch
 import os
 
-data_path = r'data'
-
 def test_data():
+    data_path = os.path.abspath(os.path.join(data_path, os.pardir, "data"))
     assert os.path.exists(data_path), f"Data path {data_path} does not exist"
-    assert len(os.listdir(data_path)) == 10, f"Data path {data_path} does not contain 10 folders"
+    assert len(os.listdir(data_path)) == 2, f"Data path {data_path} does not contain 2 folders"
     for folder in os.listdir(data_path):
         assert len(os.listdir(os.path.join(data_path, folder))) > 0, f"Folder {folder} is empty"
 
