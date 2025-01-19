cf6a52d6a4b215f1763cc225a4d0922443331cb1
jyanespu <javieryanespul@gmail.com>
2025-01-19 14:28:27 +0100

    Integration Hydra, wandb, boilerplate

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ import os
 import matplotlib.pyplot as plt
 from explainability import compute_gradcam, overlay_gradcam_on_image
 
+
 def preprocess_image(image_path, input_size, mean, std):
     image = Image.open(image_path).convert("RGB")
     image = image.resize(input_size, Image.BILINEAR)
@@ -19,11 +20,13 @@ def preprocess_image(image_path, input_size, mean, std):
     image = torch.tensor(image.transpose(2, 0, 1), dtype=torch.float32)
     return image.unsqueeze(0)
 
+
 def load_model(model_path, model_name, num_classes):
     model = timm.create_model(model_name, pretrained=False, num_classes=num_classes)
     model.load_state_dict(torch.load(model_path))
     model.eval()
     return model
+
 
 def main():
     st.title("Bird Classifier with Saliency Map")
@@ -34,26 +37,26 @@ def main():
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Bird Image.", use_container_width=True)
 
-        input_size = (224, 224)  
+        input_size = (224, 224)
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
         input_tensor = preprocess_image(uploaded_file, input_size, mean, std)
 
         model_name = "resnet50"
-        num_classes = 5 # TBM
-        model = load_model("../model.pth", model_name, num_classes)  
+        num_classes = NUM_CLASSES
+        model = load_model("../model.pth", model_name, num_classes)
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         input_tensor = input_tensor.to(device)
         model = model.to(device)
 
-        input_tensor.requires_grad_()  
+        input_tensor.requires_grad_()
         with torch.no_grad():
             output = model(input_tensor)
 
         probabilities = torch.nn.functional.softmax(output, dim=1)
         top5_probabilities, top5_classes = torch.topk(probabilities, 5)
 
-        class_names = os.listdir('../data')[:num_classes]
+        class_names = os.listdir("../data")[:num_classes]
         class_names = [name[4:].replace("_", " ") for name in class_names]
 
         top5_classes = top5_classes.cpu().numpy().flatten()
@@ -61,15 +64,15 @@ def main():
 
         st.write("Top 5 Predictions:")
         for i in range(5):
-            st.write(f"{class_names[top5_classes[i]]}: {top5_probabilities[i]*100:.2f}%")
+            st.write(f"{class_names[top5_classes[i]]}: {top5_probabilities[i] * 100:.2f}%")
 
         # Create a bar plot
         fig, ax = plt.subplots()
-        ax.bar(range(5), [top5_probabilities[i]*100 for i in range(5)], color='skyblue')
+        ax.bar(range(5), [top5_probabilities[i] * 100 for i in range(5)], color="skyblue")
         ax.set_xticks(range(5))
         ax.set_xticklabels([class_names[top5_classes[i]] for i in range(5)], rotation=90)
-        ax.set_ylabel('Probability (%)')
-        ax.set_title('Top 5 Predictions')
+        ax.set_ylabel("Probability (%)")
+        ax.set_title("Top 5 Predictions")
 
         st.pyplot(fig)
 
@@ -78,6 +81,7 @@ def main():
         gradcam_overlay = overlay_gradcam_on_image(image, gradcam)
 
         st.image(gradcam_overlay, caption="Grad-CAM Overlay", use_container_width=True)
+
 
 if __name__ == "__main__":
     main()
