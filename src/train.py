4b547a4e2e33980a237cb4fc397e788474de0f79
jyanespu <javieryanespul@gmail.com>
2025-01-19 14:21:25 +0100

    Integration Hydra, wandb, boilerplate

--- src/train.py
+++ src/train.py
@@ -1,44 +1,87 @@
-from tqdm import tqdm
-import torch
+import os
+import shutil
 
-def train(model, train_dataloader, val_dataloader, criterion, optimizer, device, num_epochs=5):
-    train_losses = []
-    val_losses = []
-    for epoch in tqdm(range(num_epochs), desc="Training Epochs"):
-        model.train()
-        running_loss = 0.0
-        for images, labels in tqdm(train_dataloader, desc="Training Batches"):
-            images, labels = images.to(device), labels.to(device)
+import dotenv
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig, OmegaConf
 
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            running_loss += loss.item()
-        train_losses.append(running_loss / len(train_dataloader))
-        print(f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {running_loss / len(train_dataloader):.4f}")
-        
-        model.eval()
-        val_loss = 0.0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in tqdm(val_dataloader, desc="Validation Batches"):
-                images, labels = images.to(device), labels.to(device)
-                
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                val_loss += loss.item()
-                
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        val_losses.append(val_loss / len(val_dataloader))
-        
-        print(f"Epoch [{epoch+1}/{num_epochs}], Validation Loss: {val_loss / len(val_dataloader):.4f}, Accuracy: {100 * correct / total:.2f}%")
-    return train_losses, val_losses
+import wandb
+from hydra_loggers import HydraRichLogger, get_hydra_dir_and_job_name
+
+from model import CustomClassifier
+from datasets import CustomDataModule
+
+dotenv.load_dotenv()
+logger = HydraRichLogger(level=os.getenv("LOG_LEVEL", "INFO"))
+
+
+@hydra.main(version_base=None, config_path="config", config_name="train")
+def train_model(cfg: DictConfig) -> None:
+    """Train and evaluate the model."""
+    logger.info("Starting training script")
+    logdir = cfg.logdir or get_hydra_dir_and_job_name()[0]
+    logger.info(f"Logging to {logdir}")
+    os.makedirs(f"{logdir}/checkpoints", exist_ok=True)
+    os.makedirs(f"{logdir}/wandb", exist_ok=True)
+
+    # Instantiate model and datamodule
+    logger.info("Initializing model and datamodule")
+    model = CustomClassifier(
+        backbone=cfg.model.backbone,
+        num_classes=cfg.model.num_classes,
+        pretrained=cfg.model.pretrained,
+        learning_rate=cfg.model.learning_rate,
+        optimizer=cfg.model.optimizer,
+    )
+    datamodule = CustomDataModule(
+        image_dir=cfg.datamodule.image_dir,
+        model_name=cfg.datamodule.model_name,
+        num_classes=cfg.datamodule.num_classes,
+        val_split=cfg.datamodule.val_split,
+        batch_size=cfg.datamodule.batch_size,
+        num_workers=cfg.datamodule.num_workers,
+        pin_memory=cfg.datamodule.pin_memory,
+    )
+
+    # Instantiate logger and callbacks
+    experiment_logger = hydra.utils.instantiate(cfg.experiment_logger, save_dir=logdir)
+    experiment_logger.log_hyperparams(OmegaConf.to_container(cfg))
+
+    early_stopping_callback = hydra.utils.instantiate(cfg.callbacks.early_stopping)
+    checkpoint_callback = hydra.utils.instantiate(cfg.callbacks.checkpoint, dirpath=f"{logdir}/checkpoints")
+    learning_rate_callback = hydra.utils.instantiate(cfg.callbacks.learning_rate_monitor)
+    progress_bar_callback = hydra.utils.instantiate(cfg.callbacks.progress_bar)
+
+    # Instantiate trainer
+    logger.info("Setting up trainer")
+    trainer = pl.Trainer(
+        default_root_dir=logdir,
+        logger=experiment_logger,
+        callbacks=[checkpoint_callback, early_stopping_callback, learning_rate_callback, progress_bar_callback],
+        **cfg.trainer,
+    )
+
+    if cfg.train:
+        logger.info("Starting training")
+        trainer.fit(model, datamodule=datamodule)
+
+    if cfg.evaluate:
+        logger.info("Starting evaluation")
+        results = trainer.test(model, datamodule=datamodule)
+
+    if cfg.upload_model:
+        logger.info("Saving model as artifact")
+        best_model = checkpoint_callback.best_model_path
+        shutil.copy(best_model, f"{logdir}/checkpoints/checkpoint.ckpt")
+        artifact = wandb.Artifact(
+            name=cfg.model.artifact_name,
+            type="model",
+            metadata={k.lstrip("test_"): round(v, 3) for k, v in results[0].items()},  # remove test_ prefix and round
+        )
+        artifact.add_file(f"{logdir}/checkpoints/checkpoint.ckpt")
+        experiment_logger.experiment.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+    train_model()
