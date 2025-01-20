6ca87b220e128cee728b96e07070302b0861f313
karlfindhansen <karl.find.hansen@gmail.com>
2025-01-20 13:34:01 +0100

    fixed trailing whitespaces from precommit

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,5 +7,3 @@ def test_data():
     assert len(os.listdir(data_path)) == 4, f"Data path {data_path} does not contain 2 folders"
     for folder in os.listdir(data_path):
         assert len(os.listdir(os.path.join(data_path, folder))) > 0, f"Folder {folder} is empty"
-
-
