e08dad6d45d33bc611edcee00be724593f4528a0
sterlie <sara.inbox0@gmail.com>
2025-01-21 14:42:38 +0100

    updated test_data

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,6 @@ import os
 def test_data():
     data_path = 'data'
     assert os.path.exists(data_path), f"Data path {data_path} does not exist"
-    assert len(os.listdir(data_path)) == 4, f"Data path {data_path} does not contain 2 folders"
+    assert len(os.listdir(data_path)) > 0, f"Data path {data_path} has no subfolders"
     for folder in os.listdir(data_path):
         assert len(os.listdir(os.path.join(data_path, folder))) > 0, f"Folder {folder} is empty"
